1a6065efe6b14143672c0b7d9c467c914a78530f
HM\PELUI <pernilla.lundin@hm.com>
2020-12-08 14:45:28 +0100

    Adding connection to h2 database. Add empty Repository.

--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
@@ -1,5 +1,6 @@
 package com.example.Weatherbasedcontent;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Controller
 public class WeatherbasedcontentController {
+
+    @Autowired
+    private WeatherbasedcontentRepository productRepos;
 
     @GetMapping("/dummyweather")
     public String testSMHI(RestTemplate restTemplate, Model model) {
---
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentRepository.java
@@ -0,0 +1,19 @@
+package com.example.Weatherbasedcontent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class WeatherbasedcontentRepository {
+    @Autowired
+    private DataSource dataSource;
+
+}
