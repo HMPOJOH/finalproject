f44bfa467bdd7f0ba69334bc4cafdf57c5d7c0f4
HM\POJOH <pontus.johansson@hm.com>
2020-12-08 14:55:07 +0100

    added bean that was missing for me

--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentApplication.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentApplication.java
@@ -2,6 +2,9 @@ package com.example.Weatherbasedcontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class WeatherbasedcontentApplication {
@@ -10,6 +13,10 @@ public class WeatherbasedcontentApplication {
 		SpringApplication.run(WeatherbasedcontentApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
 
 //Championi TEST
