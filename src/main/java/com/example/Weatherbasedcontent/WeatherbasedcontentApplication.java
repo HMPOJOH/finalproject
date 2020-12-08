86e93bd095b402710a85757f32c046e03915f48d
HM\POJOH <pontus.johansson@hm.com>
2020-12-08 10:45:33 +0100

    created a template

---
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Weatherbasedcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherbasedcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherbasedcontentApplication.class, args);
+	}
+
+}
