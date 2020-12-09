076a06abbff932ce4eaef352b4d3d79a01da179f
HM\PELUI <pernilla.lundin@hm.com>
2020-12-09 10:32:02 +0100

    Add method for fetching content by scenario in repository. add class "Content"

--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentRepository.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentRepository.java
@@ -13,7 +13,35 @@ import java.util.List;
 
 @Repository
 public class WeatherbasedcontentRepository {
+
     @Autowired
     private DataSource dataSource;
 
+    public List<Content> getContentList(int searchScenario) {
+        List<Content> content = new ArrayList<>();
+        searchScenario = 1; /* set to 1 during test */
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("select CONTENT.ID AS CONTENTID, CONTENT.IMAGE AS IMAGE, CONTENT.URL AS URL, SCENARIO.DESCRIPTION AS SCENARIO\n" +
+                     "FROM CONTENTBYSCENARIO \n" +
+                     "JOIN CONTENT ON CONTENT.ID = CONTENTBYSCENARIO.CONTENTID\n" +
+                     "JOIN SCENARIO ON SCENARIO.ID = CONTENTBYSCENARIO.SCENARIOID\n" +
+                     "WHERE CONTENTBYSCENARIO.ID = searchScenario")) {
+
+            while (rs.next()) {
+                content.add(rsContent(rs));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    private Content rsContent(ResultSet rs) throws SQLException {
+        return new Content(
+                rs.getInt("CONTENTID"),
+                rs.getString("IMAGE"),
+                rs.getString("URL"));
+    }
 }
