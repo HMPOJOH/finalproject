eb12493b0058a72de874513393afff74c55f9f9e
HM\TEMPSELAR <sebastian.larsson@hm.com>
2020-12-10 16:20:23 +0100

    More if & if else statements, added AU, UK, CA

--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
@@ -29,56 +29,98 @@ public class WeatherbasedcontentController {
     private int season = 0;
 
     @GetMapping("/setuppanel")
-    public String setupPanel(HttpSession session, Model model){
+    public String setupPanel(HttpSession session, Model model) {
         possibleLocations = locationRep.getLocationsList();
         departments = prmRep.getDepList();
         model.addAttribute("weather", weatherFromSMHI);
         model.addAttribute("locations", possibleLocations);
         model.addAttribute("departments", departments);
-        
+
 
         return "setuppanel";
     }
-
 
 
     @PostMapping("/index")
     public String index(SMHIDays smhidays, HttpSession session, @RequestParam String city, int department, @RequestParam(required = false, defaultValue = "18.071093") double longitude, @RequestParam(required = false, defaultValue = "59.325117") double latitude, RestTemplate restTemplate, Model model) {
         System.out.println(smhidays);
         //Location
-        longitude= locationRep.getLongitudeByCity(possibleLocations, city);
-        latitude= locationRep.getLatitudeByCity(possibleLocations, city);
+        longitude = locationRep.getLongitudeByCity(possibleLocations, city);
+        latitude = locationRep.getLatitudeByCity(possibleLocations, city);
         String countryID = locationRep.getCountryIDByCity(possibleLocations, city);
-
+        float temperature = 0;
+        int tempCategory = 0;
+        float windSpeed = 0;
+        int weatherSymbolNumber = 0;
+        int seasonIdbyDateAndCountry = 0;
+        String weatherCategory="";
+        int weatherCategoryId = 0;
+        String weatherImage="";
+        String symbolText="";
         //Call SMHI API
         System.out.println("get weather start..");
-        weatherFromSMHI = restTemplate.getForObject("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+longitude +"/lat/" + latitude+"/data.json", Weather.class);
-        System.out.println("get weather end..");
-
-        //Analyze SMHI weather for a specific day
-        //Two inputs, SMHI weather + which day to analyze
-        WeatherAnalyzerBySMHIDay weatherByDay = new WeatherAnalyzerBySMHIDay(weatherFromSMHI, smhidays);
-
-
+        if (countryID.equals("SE")) {
+            weatherFromSMHI = restTemplate.getForObject("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + longitude + "/lat/" + latitude + "/data.json", Weather.class);
+            System.out.println("get weather end..");
+            //Analyze SMHI weather for a specific day
+            //Two inputs, SMHI weather + which day to analyze
+            //get parameters
+            WeatherAnalyzerBySMHIDay weatherByDay = new WeatherAnalyzerBySMHIDay(weatherFromSMHI, smhidays);
+            temperature = weatherByDay.getTemp();
+            tempCategory = weatherByDay.getTempCategory(temperature);
+            windSpeed = weatherByDay.getWindSpeed();
+            weatherSymbolNumber = weatherByDay.getWeatherSymbolNumber();
+            seasonIdbyDateAndCountry = prmRep.getSeasonIdbyDateAndCountry(weatherByDay.getDateFromTimeSerieOfChoice(), countryID);
+            weatherCategory = weatherByDay.getWeatherCategory(weatherSymbolNumber);
+            weatherCategoryId = weatherByDay.getWeatherCategoryId(weatherSymbolNumber);
+            weatherImage = weatherByDay.getWeatherCategoryImage(weatherSymbolNumber);
+            symbolText = weatherByDay.getWeatherSymbolText(weatherSymbolNumber);
+        } else if (countryID.equals("AU")) {
+            //get parameters
+            temperature = 40;
+            tempCategory = 1;
+            windSpeed = 2;
+            weatherSymbolNumber = 1;
+            seasonIdbyDateAndCountry = 1;
+            weatherCategory = "Sunny";
+            weatherCategoryId = 1;
+            weatherImage = "https://img.icons8.com/office/30/000000/summer.png";
+            symbolText = "Clear sky";
+        }
+     else if (countryID.equals("UK")) {
         //get parameters
-        Float temperature = weatherByDay.getTemp();
-        int tempCategory = weatherByDay.getTempCategory(temperature);
-        Float windSpeed = weatherByDay.getWindSpeed();
-        int weatherSymbolNumber = weatherByDay.getWeatherSymbolNumber();
+        temperature = 4;
+        tempCategory = 3;
+        windSpeed = 2;
+        weatherSymbolNumber = 20;
+        seasonIdbyDateAndCountry = 4;
+        weatherCategory = "Rain";
+        weatherCategoryId = 2;
+        weatherImage = "https://img.icons8.com/office/30/000000/downpour.png";
+        symbolText = "Heavy rain";
+    }
 
-        int seasonIdbyDateAndCountry = prmRep.getSeasonIdbyDateAndCountry(weatherByDay.getDateFromTimeSerieOfChoice(), countryID);
-        String weatherCategory = weatherByDay.getWeatherCategory(weatherSymbolNumber);
-        int weatherCategoryId = weatherByDay.getWeatherCategoryId(weatherSymbolNumber);
-        String weatherImage = weatherByDay.getWeatherCategoryImage(weatherSymbolNumber);
-        String symbolText = weatherByDay.getWeatherSymbolText(weatherSymbolNumber);
+     else if (countryID.equals("CA")) {
+        //get parameters
+        temperature = -10;
+        tempCategory = 4;
+        windSpeed = 2;
+        weatherSymbolNumber = 26;
+        seasonIdbyDateAndCountry = 2;
+        weatherCategory = "Snow";
+        weatherCategoryId = 4;
+        weatherImage = "https://img.icons8.com/office/30/000000/snow.png";
+        symbolText = "Moderate snowfall";
+    }
+
 
         //desc, seasonid, weathersymbolid, tempid, department =
-        int scenarioId = prmRep.getScenarioId(seasonIdbyDateAndCountry,weatherCategoryId,tempCategory,department);
+        int scenarioId = prmRep.getScenarioId(seasonIdbyDateAndCountry, weatherCategoryId, tempCategory, department);
         System.out.println("scenarioId:" + scenarioId);
-        Scenario scenario = prmRep.getScenario(seasonIdbyDateAndCountry,weatherCategoryId,tempCategory,department);
+        Scenario scenario = prmRep.getScenario(seasonIdbyDateAndCountry, weatherCategoryId, tempCategory, department);
         System.out.println(scenario.getDescription());
         //test contentcall
-        List<Content> contentList = productRepos.getContentList(scenarioId,seasonIdbyDateAndCountry,department, weatherCategoryId);
+        List<Content> contentList = productRepos.getContentList(scenarioId, seasonIdbyDateAndCountry, department, weatherCategoryId);
 
         //Just to show the values - will rather be used in the Content lookup
         model.addAttribute("city", city);
@@ -93,10 +135,6 @@ public class WeatherbasedcontentController {
         model.addAttribute("tempcat", tempCategory);
         model.addAttribute("contentList", contentList);
         model.addAttribute("scenario", scenario.getDescription());
-
-
-
-
 
 
         return "index";
