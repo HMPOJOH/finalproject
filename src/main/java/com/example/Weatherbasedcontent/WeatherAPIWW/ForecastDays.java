7f010cb841f44f0240b20b6dddf85a3b27cf9a23
HM\POJOH <pontus.johansson@hm.com>
2020-12-11 13:34:20 +0100

    changed to forecastdays and added som sout in order to see whats happeing

--- src/main/java/com/example/Weatherbasedcontent/WeatherAPIWW/ForecastDays.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherAPIWW/ForecastDays.java
@@ -1,5 +1,7 @@
 package com.example.Weatherbasedcontent.WeatherAPIWW;
 
+
+
 public enum ForecastDays {
     TODAY,
     TOMORROW,
--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
@@ -56,45 +56,16 @@ public class WeatherbasedcontentController {
 
 
     @PostMapping("/index")
-    public String index(SMHIDays smhidays, HttpSession session, @RequestParam String city, int department, @RequestParam(required = false, defaultValue = "18.071093") double longitude, @RequestParam(required = false, defaultValue = "59.325117") double latitude, RestTemplate restTemplate, Model model) {
-
-
-        System.out.println(smhidays);
-        //Location
-        longitude = locationRep.getLongitudeByCity(possibleLocations, city);
-        latitude = locationRep.getLatitudeByCity(possibleLocations, city);
-        String countryID = locationRep.getCountryIDByCity(possibleLocations, city);
-        float temperature = 0;
-        int tempCategory = 0;
-        float windSpeed = 0;
-        int weatherSymbolNumber = 0;
-        int seasonIdbyDateAndCountry = 0;
-        String weatherCategory="";
-        int weatherCategoryId = 0;
-        String weatherImage="";
-        String symbolText="";
-        //Call SMHI API
-        System.out.println("get weather start..");
-
-
-            //new API
-            WeatherRoot weatherFromAPI =restTemplate.getForObject("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=0de04dc3bae5ebc08ee10c77aabe6215&units=metric", WeatherRoot.class);
-            ForecastDays forecastDays = setTempForcastDay(smhidays); //should be deleted later
-
-            analyzeWeatherAPI = new WeatherAnalyzer(weatherFromAPI, forecastDays);
-
-            temperature = analyzeWeatherAPI.getTemp();
-            tempCategory = analyzeWeatherAPI.getTempCategory(temperature);
-
-
-            windSpeed = analyzeWeatherAPI.getWindSpeed();
-
-            //Här är jag..
-            seasonIdbyDateAndCountry = prmRep.getSeasonIdbyDateAndCountry(analyzeWeatherAPI.getDateForSpecificDay(), weatherFromAPI.getCity().getCountry());
-
-            weatherCategoryId = analyzeWeatherAPI.getWeatherCategory();
-            weatherImage = analyzeWeatherAPI.getWeatherCategoryImage(); //http://openweathermap.org/img/wn/10d@2x.png
-            symbolText = analyzeWeatherAPI.getWeatherSymbolText();
+    public String index(ForecastDays forecastdays, HttpSession session, @RequestParam String city, int department, @RequestParam(required = false, defaultValue = "18.071093") double longitude, @RequestParam(required = false, defaultValue = "59.325117") double latitude, RestTemplate restTemplate, Model model) {
+        WeatherRoot weatherFromAPI =restTemplate.getForObject("https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=0de04dc3bae5ebc08ee10c77aabe6215&units=metric", WeatherRoot.class);
+        analyzeWeatherAPI = new WeatherAnalyzer(weatherFromAPI, forecastdays);
+        float temperature = analyzeWeatherAPI.getTemp();
+        int   tempCategory = analyzeWeatherAPI.getTempCategory(temperature);
+        float  windSpeed = analyzeWeatherAPI.getWindSpeed();
+        int  seasonIdbyDateAndCountry = prmRep.getSeasonIdbyDateAndCountry(analyzeWeatherAPI.getDateForSpecificDay(), weatherFromAPI.getCity().getCountry());
+        int weatherCategoryId = analyzeWeatherAPI.getWeatherCategory();
+        String  weatherImage = analyzeWeatherAPI.getWeatherCategoryImage(); //http://openweathermap.org/img/wn/10d@2x.png
+        String  symbolText = analyzeWeatherAPI.getWeatherSymbolText();
 
 
         //desc, seasonid, weathersymbolid, tempid, department =
@@ -106,19 +77,17 @@ public class WeatherbasedcontentController {
 
         //Just to show the values - will rather be used in the Content lookup
         model.addAttribute("city", city);
-
         model.addAttribute("temperature", temperature);
         model.addAttribute("currentWindSpeed", windSpeed);
-        model.addAttribute("weatherSymbolNumber", weatherSymbolNumber);
-        model.addAttribute("weatherCategory", weatherCategory);
         model.addAttribute("currentWeatherImage", weatherImage);
         model.addAttribute("currentWeatherSymbolText", symbolText);
-        model.addAttribute("country", countryID);
+        model.addAttribute("country", weatherFromAPI.getCity().getCountry());
         model.addAttribute("tempcat", tempCategory);
+        System.out.println("tempcat" + tempCategory);
         model.addAttribute("contentList", contentList);
         model.addAttribute("scenario", scenario.getDescription());
-
-
+        System.out.println("scenarioid" + scenario.getId());
+        System.out.println("weathercat" + weatherCategoryId);
         return "index";
     }
 
