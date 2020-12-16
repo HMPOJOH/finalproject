5a0dc8f9f4f1d22cc2551240a8108e1870f5f4f0
HM\POJOH <pontus.johansson@hm.com>
2020-12-16 16:44:37 +0100

    just moved the service class from repos folder

--- src/main/java/com/example/Weatherbasedcontent/ScenarioService.java
+++ src/main/java/com/example/Weatherbasedcontent/ScenarioService.java
@@ -1,9 +1,14 @@
-package com.example.Weatherbasedcontent.Repositories;
+package com.example.Weatherbasedcontent;
+
+import com.example.Weatherbasedcontent.Repositories.Scenario;
 
 import java.util.List;
 
 public class ScenarioService {
-
+//This service was added late in the process. Perhaps we should call it ParameterService
+// and have the ScenarioList inside this Service. Rather autowire param repos inside this And pass forward in most cases
+    //but in other cases have methods like below. Now when adding below method it's a bit tricky to follow the steps
+    //also added qty in the service class pretty late..
     private List<Scenario> scenarios;
 
 
