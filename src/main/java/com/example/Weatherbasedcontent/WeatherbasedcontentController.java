c363d4b3bdddab92e57d02817b3f9238185c522a
HM\POJOH <pontus.johansson@hm.com>
2020-12-20 13:46:28 +0100

    testing new setup

--- src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
+++ src/main/java/com/example/Weatherbasedcontent/WeatherbasedcontentController.java
@@ -148,4 +148,78 @@ public class WeatherbasedcontentController {
         model.addAttribute("scenarios", scenarios);
         return "scenarios";
     }
+
+    @GetMapping("/{city}/{department}/{forecastdays}")
+    public String view(@PathVariable ForecastDays forecastdays, HttpSession session, @PathVariable String city,@PathVariable Integer department,  RestTemplate restTemplate, Model model) {
+        //default values if API is down
+        float temperature = 5.1f;
+        int   tempCategory = 5; //avg
+        float  windSpeed = 1.2f;
+        int  seasonIdbyDateAndCountry = 2; //winter
+        int weatherCategoryId = 6; //all clouds
+        String  weatherImage = "http://openweathermap.org/img/wn/03d@2x.png"; //http://openweathermap.org/img/wn/10d@2x.png
+        String  weatherDesc = "Clouds";
+        String country="";
+
+        try {
+            WeatherRoot weatherFromAPI = restTemplate.getForObject("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=0de04dc3bae5ebc08ee10c77aabe6215&units=metric", WeatherRoot.class);
+            analyzeWeatherAPI = new WeatherAnalyzer(weatherFromAPI, forecastdays);
+            temperature = analyzeWeatherAPI.getTemp();
+            tempCategory = analyzeWeatherAPI.getTempCategory(temperature);
+            windSpeed = analyzeWeatherAPI.getWindSpeed();
+            seasonIdbyDateAndCountry = prmRep.decideSeason(analyzeWeatherAPI.getDateForSpecificDay(), weatherFromAPI.getCity().getCountry(), tempCategory);
+            weatherCategoryId = analyzeWeatherAPI.getWeatherCategory();
+            weatherImage = analyzeWeatherAPI.getWeatherCategoryImage(); //http://openweathermap.org/img/wn/10d@2x.png
+            weatherDesc = analyzeWeatherAPI.getWeatherDesc();
+            country=weatherFromAPI.getCity().getCountry();
+        }
+        catch (Exception e){
+            System.out.println("API down, using default values for demo purpose");
+        }
+
+        //desc, seasonid, weathersymbolid, tempid, department =
+        Scenario scenario = prmRep.getScenarioByValues(seasonIdbyDateAndCountry, weatherCategoryId, tempCategory, department);
+        List<Content> contentList = contentRepos.getContentList(scenario.getId(), seasonIdbyDateAndCountry, department, weatherCategoryId);
+
+        //Just to show the values - will rather be used in the Content lookup
+        model.addAttribute("city", city);
+        model.addAttribute("temperature", temperature);
+        model.addAttribute("currentWindSpeed", windSpeed);
+        model.addAttribute("currentWeatherImage", weatherImage);
+        model.addAttribute("weatherdesc", weatherDesc);
+        model.addAttribute("country", country);
+        model.addAttribute("tempcat", tempCategory);
+        model.addAttribute("weatherCategoryId", weatherCategoryId);
+        model.addAttribute("contentList", contentList );
+        model.addAttribute("scenario", scenario.getDescription());
+        model.addAttribute("background", scenario.getBackground());
+
+        System.out.println("------ Content list id--------");
+        for (Content id:contentList)
+            System.out.println(id.getId() + " " + id.getText());
+        System.out.println("------Weather parameter-----------");
+        System.out.println("country" + country);
+        System.out.println("seasonid: "+seasonIdbyDateAndCountry );
+        System.out.println("weathercat:" + weatherCategoryId);
+        System.out.println("tempcat" + tempCategory);
+        System.out.println("departmentid: "+ department);
+        System.out.println("------Calculated scenario-----------");
+        System.out.println("scenarioid: " + scenario.getId());
+        System.out.println("scenario desc: " + scenario.getDescription());
+        model.addAttribute("forecastday", forecastdays);
+        return "viewpanel";
+    }
+
+
+    @GetMapping("/views/{city}")
+    public String setupPanel(HttpSession session, Model model,@PathVariable String city) {
+
+        departments = prmRep.getDepList();
+
+
+        model.addAttribute("departments", departments);
+
+        return "demoview";
+    }
+
 }
